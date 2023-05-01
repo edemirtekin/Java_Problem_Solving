8ab5a7569a9d6f6d1a7d5a62146cf3ff30b77779
Emre Demirtekin <edemirtekin11@gmail.com>
2023-05-01 14:42:45 +0300

    01.05

--- src/main/java/Q25.java
+++ src/main/java/Q25.java
@@ -1,9 +1,23 @@
-public class Kalip {
+import java.util.Arrays;
 
-    // (TR)
-    // (EN)
+public class Q25 {
+
+    // (TR) // Biri hariç m'den n'ye tüm tek sayıları içeren m'den n'ye kadar bir pozitif tamsayı dizimiz olduğunu
+    // varsayalım. Listede eksik olan tek sayıyı bulan bir fonksiyon yazınız.
+
+    // (EN) Assume that we have an array of positive integers from m to n which includes al odd numbers from
+    // m to n except one. Write a function that finds the missing odd number in the list.
 
     public static void main(String[] args) {
+
+        int[] arr={9,7,3};
+        Arrays.sort(arr);
+
+        for (int i=0; i< arr.length-1; i++){
+            if ((arr[i+1] - arr[i])!=2){
+                System.out.println("missing number: " + (arr[i]+2));
+            }
+        }
 
 
 
--- src/main/java/Q26.java
+++ src/main/java/Q26.java
@@ -1,9 +1,21 @@
-public class Kalip {
+public class Q26 {
 
-    // (TR)
-    // (EN)
+    // (TR) Yalnızca 'A' ve 'B' karakterlerinden oluşan bir string ifade olan str verildiğinde, 'A' ve 'B' nin görünüm
+    // sayıları arasındaki farkı bulan kodu yazınız.
+
+    // (EN) Given a string str which only consists of characters 'A' and 'B', find the absolute difference
+    // between their number of occurrences.
 
     public static void main(String[] args) {
+
+        String str = "AABAABAA";
+        int onlyA = str.replaceAll("B","").length();
+        int onlyB = str.replaceAll("A","").length();
+
+        int diff = Math.abs(onlyA-onlyB);
+
+        System.out.println(diff);
+
 
 
 
